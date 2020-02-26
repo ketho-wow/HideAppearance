4525ee3e28c79ed1fc28229be52bc69b5ee6fc1a
Ketho <sghs17@outlook.com>
2020-02-26 04:32:18 +0100

    Added toggle for showing hidden Appearances Fixed dropdown not showing for unlearned Appearances

--- HideAppearance.lua
+++ HideAppearance.lua
@@ -1,9 +1,10 @@
+local showHidden
 local GetCategoryAppearances = C_TransmogCollection.GetCategoryAppearances
 
 -- probably not the right way to do this and taints everything
 function C_TransmogCollection.GetCategoryAppearances(...)
 	local visualsList = GetCategoryAppearances(...)
-	if HideAppearanceDB then
+	if HideAppearanceDB and not showHidden then
 		-- iterate from end to beginning for tremove
 		for i = #visualsList, 1, -1 do
 			if HideAppearanceDB[visualsList[i].visualID] then
@@ -30,10 +31,20 @@ f:SetScript("OnEvent", f.OnEvent)
 
 function f.AddHideButton(model, button)
 	if button == "RightButton" then
-		UIDropDownMenu_AddButton({notCheckable = true, disabled = true})
-		UIDropDownMenu_AddButton({notCheckable = true, text = HIDE, func = function()
-			f:HideTransmog(model)
-		end})
+		if not DropDownList1:IsShown() then -- force show dropdown
+			WardrobeModelRightClickDropDown.activeFrame = model
+			ToggleDropDownMenu(1, nil, WardrobeModelRightClickDropDown, model, -6, -3)
+		end
+		UIDropDownMenu_AddButton({notCheckable = true, disabled = true}) -- empty space
+		if not showHidden then -- only show Hide option when actively filtering
+			UIDropDownMenu_AddButton({notCheckable = true, text = HIDE, func = function() f:HideTransmog(model) end})
+		end
+		-- allow toggling hiding
+		local info = UIDropDownMenu_CreateInfo()
+		info.text = "Show hidden"
+		info.checked = function() return showHidden end
+		info.func = function() showHidden = not showHidden; f:UpdateWardrobe() end
+		UIDropDownMenu_AddButton(info)
 	end
 end
 
@@ -42,7 +53,11 @@ function f:HideTransmog(model)
 	local source = WardrobeCollectionFrame_GetSortedAppearanceSources(visualID)[1]
 	local name, link = GetItemInfo(source.itemID)
 	HideAppearanceDB[visualID] = name
+	self:UpdateWardrobe()
+	print("Hiding "..link.." from the Appearances Tab")
+end
+
+function f:UpdateWardrobe()
 	WardrobeCollectionFrame.ItemsCollectionFrame:RefreshVisualsList()
 	WardrobeCollectionFrame.ItemsCollectionFrame:UpdateItems()
-	print("Hiding "..link.." from the Appearances Tab")
 end
